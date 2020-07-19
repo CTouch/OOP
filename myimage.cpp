eb70aa92538dd53374c9bf1fd13a90457ebe0c82
CTouch <953918409@qq.com>
2020-07-19 19:16:58 +0800

    add some about hist

--- myimage.cpp
+++ myimage.cpp
@@ -1,9 +1,18 @@
 #include "myimage.h"
 
+
 myImage::myImage()
 {
 }
 
+myImage::myImage(int width,int height,QImage::Format format)
+    :img(width,height,format)
+{
+    img.fill(Qt::black);
+    shape = Shape::IMAGE;
+    graph_width = img.width();
+    graph_height = img.height();
+}
 myImage::myImage(int Index,const QString & FilePath):img(FilePath)
 {
     this->Index = Index;
@@ -14,6 +23,10 @@ myImage::myImage(int Index,const QString & FilePath):img(FilePath)
     Center = QPoint(graph_width / 2, graph_height / 2); // 图形的中心点
     border_tl = QPoint(0,0); // 矩形辅助边框的左上角, top left, 未经过旋转和放缩
     border_br = QPoint(graph_width,graph_height); // 矩形辅助边框的右下角, bottom right, 未经过旋转和放缩
+
+    std::cout << "begin show hist!\n";
+    showHist();
+
 }
 
 myImage::myImage(int Index,QDataStream &in)
@@ -77,4 +90,52 @@ void myImage::read(QDataStream &in)
     in >> border_tl >> border_br;
 
     in >> img;
+}
+
+int myImage::GetMax(int a[],int len)
+{
+    int result = a[0];
+    for(int i = 1;i < len;i++)
+    {
+        if(result < a[i]) result = a[i];
+    }
+    return result;
+}
+
+//void myImage::
+void myImage::showHist()
+{
+    RGBHist res;
+    for(int i = 0;i < img.width();i++)
+    {
+        for(int j = 0;j < img.height();j++)
+        {
+            QRgb tp = img.pixel(i,j);
+            int red = qRed(tp);
+            int green = qGreen(tp);
+            int blue = qBlue(tp);
+            res.r[red]++;
+            res.g[green]++;
+            res.b[blue]++;
+        }
+    }
+    int max_red = GetMax(res.r,MAX_VALUE_COLOR);
+
+    myImage tp(256,256,QImage::Format_ARGB32);
+//    QImage tp(256,256,QImage::Format_ARGB32);
+//    tp.fill(Qt::black);
+    QPainter painter(&(tp.img));
+    painter.setPen(Qt::red);
+    for(int i = 0;i < 256;i++)
+    {
+        painter.drawLine(i,0,i,256.0 * res.r[i] / max_red);
+//        std::cout << res.r[i] << "\t";
+//        if(i%10 == 0)  std::cout << std::endl;
+    }
+//    std::cout << "try save hist!\n";
+//    if (tp.img.save("~/testhist.bmp","BMP"))
+//        std::cout << "Save hist!\n";
+//    else std::cout <<"failed to save hist!\n";
+    painter.end();
+
 }
--- myimage.h
+++ myimage.h
@@ -2,11 +2,33 @@
 #define MYIMAGE_H
 #include <QImage>
 #include <QPainter>
+#include <QRgb>
+#include <iostream>
+#include <vector>
+//#include <QCustomPlot>
 #include "graph.h"
+
+//the max value of r/g/b
+#define MAX_VALUE_COLOR 256
+class RGBHist
+{
+public:
+    RGBHist(){};
+    ~RGBHist(){};
+//    QVector<int> r(256);
+//    std::vector<int> r(256);
+    int r[MAX_VALUE_COLOR] = {0};
+    int g[MAX_VALUE_COLOR] = {0};
+    int b[MAX_VALUE_COLOR] = {0};
+private:
+};
+
 class myImage:public Graph
 {
 public:
     myImage();
+    //initial a (width * height) image filled with black
+    myImage(int width,int height,QImage::Format);
 
     //load image from .png file
     myImage(int index,const QString & FliePath);
@@ -16,12 +38,14 @@ public:
 //    void draw(QPainter &painter) override;
 public:
     virtual void draw(QPainter &painter) override; // 绘制
+    virtual void showHist() override;
     virtual Graph *duplicate() override; // 复制
     virtual void read(QDataStream &in) override; // 文件的读
     virtual void write(QDataStream &out) override; // 文件的写
 private:
     QImage img;
 
+    int GetMax(int a[],int len);
 
 protected:
 
